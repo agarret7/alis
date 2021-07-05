20f3ca1e1160e15092c66213da8e15b91b4ebefc
Austin Garrett <agarret7@mit.edu>
2021-07-04 22:08:02 -0700

    Add ssh keygen procedure before installing git packages

--- alis-config.sh
+++ alis-config.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
-# install packages
+
+# install system packages
 ./alis-packages.sh
+
+# create ssh key (will prompt password)
+ssh-keygen -t ed25519 -C "agarrett777@gmail.com" -f $HOME/.ssh/id_ed25519
+echo "PUB KEY: $(cat)"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+echo "Add your key to github and press any key to continue..."
+read
+
+# install git packages
 ./alis-git.sh
 
-# refresh terminal
+# refresh system with configuration updated
+i3-msg reload
 xrdb ~/.Xresources
 exec bash
