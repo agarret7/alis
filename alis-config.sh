2d4b73aaeab99be19f73a92855ef9f05df649870
Austin Garrett <agarret7@mit.edu>
2021-07-04 22:10:37 -0700

    Frontload interactive (ssh keygen) procedures

--- alis-config.sh
+++ alis-config.sh
@@ -2,9 +2,6 @@
 set -e
 
 
-# install system packages
-./alis-packages.sh
-
 # create ssh key (will prompt password)
 ssh-keygen -t ed25519 -C "agarrett777@gmail.com" -f ~/.ssh/id_ed25519
 eval "$(ssh-agent -s)"
@@ -13,10 +10,11 @@ echo "PUB KEY: $(cat ~/.ssh/id_ed25519)"
 echo "Add your key to github and press any key to continue..."
 read
 
-# install git packages
+# install system packages
+./alis-packages.sh
 ./alis-git.sh
 
-# refresh system with configuration updated
+# refresh system with new config
 i3-msg reload
 xrdb ~/.Xresources
 exec bash
